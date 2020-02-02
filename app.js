995ecc9609b1a035130c56dbea940382b778200d
cnfreelancer.com <9570075@qq.com>
2020-02-02 16:26:54 +0800

    database create

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //引入http模块
-const http=require('http');
+const http = require('http');
+
+require('./model/connect')
+
+const Student=require('./model/user')
 
 //创建网站服务器
-const app= http.createServer();
+const app = http.createServer();
+
+
 
 //当客户端访问服务器端的时候
-app.on('request',(req,res)=>{
+app.on('request', (req, res) => {
     res.end('OK');
 });
 
